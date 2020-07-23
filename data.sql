10a305dfd8f995820925e8d8e2888130f936cd60
Ragil Arain Sawala <arainsyawala@gmail.com>
2020-07-24 00:54:45 +0700

    Add file README.MD

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO T1_POSITION(id, code, name, is_delete) values (1, PRG, Programmer, 0);
+INSERT INTO T1_POSITION(id, code, name, is_delete) values (2, BPR, BPR, 0);
+INSERT INTO T1_POSITION(id, code, name, is_delete) values (3, TEST, Tester, 0);
+INSERT INTO T1_POSITION(id, code, name, is_delete) values (4, HELP, Help Desk, 0);
+INSERT INTO T1_POSITION(id, code, name, is_delete) values (5, STF, Staff, 0);
